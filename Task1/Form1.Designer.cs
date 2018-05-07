9c94df0078fb4719e4a45bfcc9b68edd01e4d7c2
Дмитрий Санососюк <smittych@mail.ru>
2018-05-07 19:59:25 +0300

    Add project files.

---
+++ Task1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Task1
+{
+    partial class Task1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstPop = new System.Windows.Forms.ListBox();
+            this.lstPush = new System.Windows.Forms.ListBox();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstPop
+            // 
+            this.lstPop.FormattingEnabled = true;
+            this.lstPop.Location = new System.Drawing.Point(146, 41);
+            this.lstPop.Name = "lstPop";
+            this.lstPop.Size = new System.Drawing.Size(120, 212);
+            this.lstPop.TabIndex = 7;
+            // 
+            // lstPush
+            // 
+            this.lstPush.FormattingEnabled = true;
+            this.lstPush.Location = new System.Drawing.Point(20, 41);
+            this.lstPush.Name = "lstPush";
+            this.lstPush.Size = new System.Drawing.Size(120, 212);
+            this.lstPush.TabIndex = 6;
+            // 
+            // btnPop
+            // 
+            this.btnPop.Location = new System.Drawing.Point(146, 10);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(75, 23);
+            this.btnPop.TabIndex = 5;
+            this.btnPop.Text = "pop";
+            this.btnPop.UseVisualStyleBackColor = true;
+            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(19, 10);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(75, 23);
+            this.btnPush.TabIndex = 4;
+            this.btnPush.Text = "push";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // Task1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lstPop);
+            this.Controls.Add(this.lstPush);
+            this.Controls.Add(this.btnPop);
+            this.Controls.Add(this.btnPush);
+            this.Name = "Task1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstPop;
+        private System.Windows.Forms.ListBox lstPush;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Button btnPush;
+    }
+}
+
---
+++ Task1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace Task1
+{
+    public partial class Task1 : Form
+    {
+        public int itemId;
+        public TestQueue testQueue;
+        //==================================================================================================================
+        public Task1()
+        {
+            testQueue = new TestQueue();
+            testQueue.queue = new Queue<object>();
+            testQueue.onPop += new Action<object>(testQueue_onPop);
+
+            itemId = 0;
+            InitializeComponent();
+        }
+        //==================================================================================================================
+        void testQueue_onPop(object obj)
+        {
+            Action action = () =>
+            {
+                lstPop.Items.Add((int)obj);
+                FillPushList();
+                btnPop.Enabled = true;
+            };
+            Invoke(action);
+        }
+        //==================================================================================================================
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            testQueue.Push(itemId);
+            itemId++;
+
+            FillPushList();
+        }
+        //==================================================================================================================
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            testQueue.Pop();
+            btnPop.Enabled = false;
+        }
+        //==================================================================================================================
+        public void FillPushList()
+        {
+            lstPush.Items.Clear();
+            for (int i = 0; i < testQueue.queue.Count; i++)
+            {
+                lstPush.Items.Add(testQueue.queue.ElementAt(i));
+            }
+        }
+        //==================================================================================================================
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            testQueue.isStop = true;
+        }
+        //==================================================================================================================
+    }
+
+    public class TestQueue
+    {
+        public Queue<object> queue { get; set; }
+        public event Action<object> onPop;
+        public bool isStop;
+        //========================================================================================================
+        public TestQueue()
+        {
+            isStop = false;
+            queue = new Queue<object>();
+        }
+        //========================================================================================================
+        public void Push(object item)
+        {
+            queue.Enqueue(item);
+        }
+        //========================================================================================================
+        public void Pop1()
+        {
+            onPop(queue.Dequeue());
+        }
+        //========================================================================================================
+        public void Pop(bool isStop = false)
+        {
+            Thread _threadPop = new Thread(thread_Pop);
+            _threadPop.Start();
+        }
+        //========================================================================================================
+        public void thread_Pop()
+        {
+            if (queue.Count == 0)
+            {
+                while (true)
+                {
+                    if (isStop) return;
+                    if (queue.Count == 0) continue;
+
+                    onPop(queue.Dequeue());
+                    return;
+                }
+            }
+            onPop(queue.Dequeue());
+        }
+        //========================================================================================================
+    }
+}
